4599c165cec76bdfaa6e2fe5dd73c5f37a92fc24
Chuks Ajeh <chuksajeh1@gmail.com>
2020-05-12 14:38:06 +0100

    More solved problems

---
+++ EasyProblems/Algorithms/QueuesViaStacks.java
@@ -0,0 +1,54 @@
+import java.util.Stack;
+
+public class MyQueue {
+
+    /** Initialize your data structure here. */
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> holder = new Stack<>();
+    public MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack.size()<1){
+            throw new NullPointerException();
+        }
+        while(stack.size()>1){
+            holder.push(stack.pop());
+        }
+        int val = stack.pop();
+        while(!holder.empty()){
+            stack.push(holder.pop());
+        }
+        return val;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(stack.size()<1){
+            throw new NullPointerException();
+        }
+        while(stack.size()>1){
+            holder.push(stack.pop());
+        }
+        int val = stack.pop();
+
+        holder.push(val);
+        while(!holder.empty()){
+            stack.push(holder.pop());
+        }
+        return val;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.size() == 0;
+    }
+
+}
